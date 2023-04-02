190a34986a8d0f9bf2ee5aed8070f028af319e4f
ayoub3bidi <mrayoubabidi@gmail.com>
2023-04-02 17:27:11 +0100

    ✨ feat: add loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-[#03001C] sm:px-8 px-4 py-4">
       <Link to="/">
-        <h1 class="inline-block bg-gradient-to-r from-[#B6EADA] via-[#5B8FB9] to-[#301E67] bg-clip-text text-bold text-3xl text-transparent font-bold">IMAGINE</h1>
+        <h1 className="inline-block bg-gradient-to-r from-[#B6EADA] via-[#5B8FB9] to-[#301E67] bg-clip-text text-bold text-3xl text-transparent font-bold">IMAGINE</h1>
       </Link>
       <Link to="/create-post" className="font-inter font-medium bg-[#301E67] text-white px-4 py-2 rounded-md">Create</Link>
     </header>
