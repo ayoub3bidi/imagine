8ea5c38b82c0c5d7df1bc3c78abec5f58a53c5e5
ayoub3bidi <mrayoubabidi@gmail.com>
2023-04-03 22:32:02 +0100

    feat: initiate create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {preview} from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImage, setGeneratingImage] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-white text-[32px]">Create</h1>
+        <p className="mt-2 text-white text-[14px] max-w-[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
+      </div>
+    </section>
   )
 }
 
