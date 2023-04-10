f8ebbb1ea50cfcb4c114a7fb9df5edf71133b494
ayoub3bidi <mrayoubabidi@gmail.com>
2023-04-10 18:47:23 +0100

    ✨ feat: add mongo db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 
 const startServer = async () => {
     try {
+        connectDB(process.env.MONGODB_URL)
         app.listen(port, () => {
         console.log(`Server started on port ${port}`);
         });
