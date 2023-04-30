7af13a4a75c293dd4cef64fa12fcbd5c7e3a25e0
ayoub3bidi <mrayoubabidi@gmail.com>
2023-04-30 10:47:10 +0100

    ✨ feat: integrating backend to the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Loader, FormField, Card } from '../components';
+
+import { Card, FormField, Loader } from '../components';
 
 const Cards = ({ data, title }) => {
   if (data?.length > 0) {
@@ -9,7 +10,7 @@ const Cards = ({ data, title }) => {
   }
 
   return (
-    <h2 className="mt-5 font-bold text-white text-xl uppercase">{title}</h2>
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
   );
 };
 
@@ -21,10 +22,42 @@ const Home = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
   };
 
   return (
